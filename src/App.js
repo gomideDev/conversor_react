d445cbc0c2af2c832147e9aef25ae8a754dac0b1
Silas Sampaio <silas.dev@hotmail.com>
2019-12-13 18:12:32 -0300

    commit final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import Conversor from './components/Conversor'
 function App() {
   return (
     <div className="App">
-      <Conversor moedaA='CAD' moedaB='BLR'/>
+      <h1>Conversor de Moedas</h1>
+      <div className="line">
+        <Conversor moedaA='CAD' moedaB='BRL'/>
+        <Conversor moedaA='BRL' moedaB='CAD'/>
+      </div>
+      <div className="line">
+        <Conversor moedaA='USD' moedaB='BRL'/>
+        <Conversor moedaA='BRL' moedaB='USD'/>
+      </div>
+      <div className="line">
+        <Conversor moedaA='EUR' moedaB='BRL'/>
+        <Conversor moedaA='BRL' moedaB='EUR'/>
+      </div>
     </div>
   );
 }
--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import './Conversor.css'
 
 export default class Conversor extends Component {
 
@@ -7,22 +9,29 @@ export default class Conversor extends Component {
 
         this.state = {
             moedaA_valor: '',
-            moedaB_valor: 0
+            moedaB_valor: 0,
+            valor_convertido: 0
         }
         this.converter = this.converter.bind(this);
     }
 
 converter(){
-    console.log(this.state)
+  let url = 'https://api.exchangerate-api.com/v4/latest/'+this.props.moedaA;
+  Axios.get(url).then((result)=>{
+    console.log(result.data.rates[this.props.moedaB])
+    let cotacao = parseFloat(result.data.rates[this.props.moedaB]);
+    let moedaB_valor = (this.state.moedaA_valor * cotacao).toFixed(2)
+    this.setState({moedaB_valor})
+  });
 }
 
   render() {
     return (
       <div className='conversor'>
   <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-          <input type="text" onChange={(event)=>{this.setState({moedaA_valor: event.target.value})}}/>
+          <input type="text" value={this.state.moedaA_valor} onChange={(event)=>{this.setState({moedaA_valor: event.target.value})}}/>
           <input type="button" onClick={this.converter} value='Converter'/>
-          <h2>Valor convertido</h2>
+          <h2>{this.state.moedaB_valor}</h2>
       </div>
     );
   }
